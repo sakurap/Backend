21a897f719edf5faefb7829fbe9fb0a5d676b9a0
nipat <siramest@gmail.com>
2020-12-12 10:06:19 -0500

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3881553991908615',
+        clientSecret: '7e54687fb03c8ced945067b8f7d1306b'
+    }
 }
